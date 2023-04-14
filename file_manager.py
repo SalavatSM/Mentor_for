4a0f507d53fca85d1765b69d868806442d1fe921
SalavatSM <salavatfromkg@gmail.com>
2023-04-14 11:17:33 +0600

    add file_manager v2

--- file_manager.py
+++ file_manager.py
@@ -1,2 +1,25 @@
-class Work_with_files:
-    pass
+class FileManager:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def create_file(self):
+        with open(self.filename, 'w') as f:
+            pass  # Тут создается файл без записи в него. Запись в методе далее.
+
+    def write_to_file(self, text):
+        with open(self.filename, 'w') as f:
+            f.write(text)
+
+    def delete_file(self):
+        import os
+        try:
+            os.remove(self.filename)
+            print(f"{self.filename} deleted successfully")
+        except OSError as e:
+            print(f"Error deleting file: {e.filename} - {e.strerror}")
+
+if __name__ == '__main__':
+    fm = FileManager('example.txt')
+    fm.create_file()
+    fm.write_to_file('Hello, world!')
+    fm.delete_file()
